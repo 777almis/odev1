248ae97418dd0e373cf71eccd5e3a596a6bc7574
777almis <156001603+777almis@users.noreply.github.com>
2024-01-08 21:50:59 +0300

    Add files via upload
    2024 deneyap 1

---
+++ ödev1_2024.cpp
@@ -0,0 +1,25 @@
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int ortalama;
+ say<<"Lutfen iki yazilinizin da sonuçlarýný girin!"<<endl;
+  cin>>ortalama;
+ if(ortalamaniz>50){
+     find<<"Dersi geçtiniz!"<<endl;
+ }    
+ else{
+     find<<"Dersten kaldiniz!"<<endl;
+ }
+}
